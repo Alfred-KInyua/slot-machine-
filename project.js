69a9489d9395897ccddc1b15fb5483c2a6e5bf67
Alfred-KInyua <alfkinyua@gmail.com>
2025-10-01 11:50:01 +0300

    Capture user deposit

--- project.js
+++ project.js
@@ -1,6 +1,19 @@
 const prompt = require ("prompt-sync")(); 
+let count=3;
 const deposit =()=>{
+    while(count>0){
     const deposit_amount= prompt("Enter a deposit amount: ");
-
+    const number_deposit_amount= parseFloat(deposit_amount);
+    if (isNaN(number_deposit_amount) || number_deposit_amount<=0){
+        console.log("Invalid deposit, try again");
+    }
+     
+    else{
+        return number_deposit_amount;
+    }
+    
+  
 }
-deposit();
+}
+const des= deposit();
+console.log("Your balance is " +des+ " dollars");
