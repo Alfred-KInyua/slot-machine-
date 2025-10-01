25d24a7d443c46c717a93a929bb0adb2dac4f16f
Alfred-KInyua <alfkinyua@gmail.com>
2025-10-01 12:25:22 +0300

    Add number of lines from user

--- project.js
+++ project.js
@@ -1,19 +1,25 @@
-const prompt = require ("prompt-sync")(); 
-let count=3;
-const deposit =()=>{
-    while(count>0){
-    const deposit_amount= prompt("Enter a deposit amount: ");
-    const number_deposit_amount= parseFloat(deposit_amount);
-    if (isNaN(number_deposit_amount) || number_deposit_amount<=0){
-        console.log("Invalid deposit, try again");
+const prompt = require("prompt-sync")();
+const deposit = () => {
+  while (true) {
+    const deposit_amount = prompt("Enter a deposit amount: ");
+    const number_deposit_amount = parseFloat(deposit_amount);
+    if (isNaN(number_deposit_amount) || number_deposit_amount <= 0) {
+      console.log("Invalid deposit, try again");
+    } else {
+      return number_deposit_amount;
     }
-     
-    else{
-        return number_deposit_amount;
+  }
+};
+const get_num_of_lines = () => {
+  while (true) {
+    const lines = prompt("Enter a number of lines (1-3)");
+    const line_numbers = parseFloat(lines);
+    if (isNaN(line_numbers) || line_numbers <= 0 || line_numbers > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return line_numbers;
     }
-    
-  
-}
-}
-const des= deposit();
-console.log("Your balance is " +des+ " dollars");
+  }
+};
+const des = deposit();
+const lines = get_num_of_lines();
